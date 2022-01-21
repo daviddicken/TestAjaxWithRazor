c77ea0410ff5d2d75dc12d6a59c3b902fd65217f
David Dicken <davidfromseattle@gmail.com>
2022-01-20 19:23:22 -0800

    Add project files.

---
+++ TestAjaxWithRazor/Data/TestDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestAjaxWithRazor.Models;
+
+
+namespace TestAjaxWithRazor.Data
+{
+  public class TestDbContext
+  {
+     public DbSet<Product> DbProducts { get; set; }
+  }
+}
---
+++ TestAjaxWithRazor/Pages/SalesPage/Index.cshtml.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TestAjaxWithRazor.Models;
+
+namespace TestAjaxWithRazor.Pages.SalesPage
+{
+  public class IndexModel : PageModel
+  {
+    public void OnGet()
+    {
+    }
+
+    public IActionResult OnGetDemo1()
+    {
+      return new JsonResult("Hello");
+    }
+
+    public IActionResult OnGetDemo2(string fullName)
+    {
+      return new JsonResult("Hi " + fullName);
+    }
+
+    public IActionResult OnGetDemo3()
+    {
+      var product = new Product
+      {
+        Id = "p01",
+        Name = "name 1",
+        Price = 4.5
+      };
+      return new JsonResult(product);
+    }
+
+    public IActionResult OnGetDemo4()
+    {
+      var products = new List<Product>
+            {
+                new Product {
+                    Id = "p01",
+                    Name = "name 1",
+                    Price = 4.5
+                },
+                new Product {
+                    Id = "p02",
+                    Name = "name 2",
+                    Price = 7
+                },
+                new Product {
+                    Id = "p03",
+                    Name = "name 3",
+                    Price = 9
+                }
+            };
+      return new JsonResult(products);
+    }
+  }
+}
